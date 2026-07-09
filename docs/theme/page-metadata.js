1e5ceaf2e94577aea92ddf1d359bc5dadb031112
Your Name <you@example.com>
2026-07-10 00:32:16 +0800

    1

--- docs/theme/page-metadata.js
+++ docs/theme/page-metadata.js
@@ -1,6 +1,6 @@
 window.__BFYES_PAGE_META__ = {
   "/": {
-    "updated": "2026-07-09T22:41:33+08:00"
+    "updated": "2026-07-10T00:02:47+08:00"
   },
   "/diaries/midterm/": {
     "updated": "2026-07-09T15:03:14+08:00"
