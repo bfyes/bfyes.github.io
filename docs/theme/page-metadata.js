9237bea8d8f221194091f4bee970a78b3c17f97a
Your Name <you@example.com>
2026-07-12 00:07:33 +0800

    ok

--- docs/theme/page-metadata.js
+++ docs/theme/page-metadata.js
@@ -11,8 +11,50 @@ window.__BFYES_PAGE_META__ = {
   "/study/asm/": {
     "updated": "2026-07-10T15:44:11+08:00"
   },
+  "/study/fds/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/fds/project1/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/fds/project2/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/fds/project3/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/hpc/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/hpc/lab1/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
   "/study/ismath/": {
     "updated": "2026-07-10T15:44:11+08:00"
+  },
+  "/study/sys1/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/sys1/lab0/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/sys1/lab1/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/sys1/lab2/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/sys1/lab3/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/sys1/lab4/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/sys1/lab5/": {
+    "updated": "2026-07-11T23:43:42+08:00"
+  },
+  "/study/sys1/project/": {
+    "updated": "2026-07-11T23:43:42+08:00"
   },
   "/tools/mac\u4e0a\u597d\u7528\u7684\u7b2c\u4e09\u65b9\u8bb0\u4e8b\u672c\u8f6f\u4ef6/": {
     "updated": "2026-07-09T15:03:14+08:00"
