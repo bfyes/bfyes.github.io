f6843c7831144f8f57fc4e0059c34d859c9f83a1
Your Name <you@example.com>
2026-07-13 15:16:51 +0800

    css

--- docs/theme/page-metadata.js
+++ docs/theme/page-metadata.js
@@ -1,6 +1,6 @@
 window.__BFYES_PAGE_META__ = {
   "/": {
-    "updated": "2026-07-11T18:24:48+08:00"
+    "updated": "2026-07-13T15:08:49+08:00"
   },
   "/diaries/fa25_review/": {
     "updated": "2026-07-10T02:17:39+08:00"
